1a04ca30c0f69c84a43a5101d94ba39705125dfb
qiang.qin <qiang.qin@ly.com>
2019-01-22 17:19:02 +0800

    项目拆分

--- juzi/apps/pages/scripts/directives.js
+++
@@ -1,6 +0,0 @@
-angular.module('directives', [])
-.filter('trust2Html', ['$sce',function($sce) {  
-    return function(val) {  
-        return $sce.trustAsHtml(val);   
-    };  
-}])
--- juzi/apps/pages/scripts/public.js
+++
@@ -1,8 +0,0 @@
-// 数字补0
-function formatNumber(n) {
-	n = n.toString()
-	return n[1] ? n : '0' + n
-}
-
-// 接口请求域名
-var requestURL = 'http://api.juzi001.com:3000';
